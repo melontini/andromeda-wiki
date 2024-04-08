4406bbecb2a8abd95a616ca26a8591699019e11f
melontini <104443436+melontini@users.noreply.github.com>
2024-04-08 15:01:19 +0700

    Listen to onMounted too

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
 import giscusTalk from 'vitepress-plugin-comment-with-giscus';
-import type { Theme } from 'vitepress'
+import type { Theme, VitePressData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import { useData, useRoute } from 'vitepress';
 import { toRefs } from "vue";
-import { watch, nextTick } from 'vue';
+import { watch, nextTick, onMounted } from 'vue';
 import { GiscusProps } from '@giscus/vue/dist/types'
 
 export default {
@@ -30,25 +30,32 @@ export default {
       loading: 'eager'
     }
 
+    onMounted(() => {
+      fixLang(props, data);
+    })
     watch(() => route.path, () => nextTick(() => {
-      var lang = data.lang.value
-      switch (lang) {
-        case "en-US":
-          lang = "en"
-          break;
-        case "zh-CN":
-          lang = "zh-CN"
-          break;
-        default:
-          lang = "en"
-          break;
-      }
-      props.lang = lang
+      fixLang(props, data);
     }))
 
     // Obtain configuration from: https://giscus.app/
     giscusTalk(props, { frontmatter, route }, true);
   }
 } satisfies Theme
+
+const fixLang = (props: GiscusProps, data: VitePressData) => {
+  var lang = data.lang.value
+  switch (lang) {
+    case "en-US":
+      lang = "en"
+      break;
+    case "zh-CN":
+      lang = "zh-CN"
+      break;
+    default:
+      lang = "en"
+      break;
+  }
+  props.lang = lang
+}
 
 
