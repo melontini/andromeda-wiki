00bfa42fd2f504caf7b1e65675d865049849d711
melontini <104443436+melontini@users.noreply.github.com>
2024-04-08 14:57:18 +0700

    Enable Giscuss comments (#32)
    * Add giscus
    
    * remove console.log

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,54 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
+import { watch, nextTick } from 'vue';
+import { GiscusProps } from '@giscus/vue/dist/types'
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  setup() {
+    // Get frontmatter and route
+    const data = useData();
+    const { frontmatter } = toRefs(data);
+    const route = useRoute();
+
+    var props: GiscusProps = {
+      repo: 'melontini/andromeda-wiki',
+      repoId: 'R_kgDOJxq74g',
+      category: 'Comments',
+      categoryId: 'DIC_kwDOJxq74s4CeieV',
+      mapping: 'pathname',
+      inputPosition: 'top',
+      reactionsEnabled: '1',
+      loading: 'eager'
+    }
+
+    watch(() => route.path, () => nextTick(() => {
+      var lang = data.lang.value
+      switch (lang) {
+        case "en-US":
+          lang = "en"
+          break;
+        case "zh-CN":
+          lang = "zh-CN"
+          break;
+        default:
+          lang = "en"
+          break;
+      }
+      props.lang = lang
+    }))
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk(props, { frontmatter, route }, true);
   }
 } satisfies Theme
+
+
