709e9c163eeb970197948ec48370e9243f9d70dc
Rad <99306685+Rad586@users.noreply.github.com>
2024-05-14 14:31:09 +0800

    comma's back

--- docs/.vitepress/zh_cn.ts
+++ docs/.vitepress/zh_cn.ts
@@ -25,7 +25,7 @@ export const zh_cn = defineConfig({
                 text: "Meta",
                 items: [
                     { text: '模组介绍', link: '/zh-cn/showcases' },
-                    { text: '附件', link: '/zh-cn/assets' }
+                    { text: '附件', link: '/zh-cn/assets' },
                 ]
             }
         ],
